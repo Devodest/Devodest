ec36f5200d6bdc8fe75d4fadff94013fa09425ff
Hart <Hartik1995@mail.ru>
2022-09-30 11:38:27 +0300

    add sem7

--- Lesson/Lesson_007/calc_seminar/check.py
+++ Lesson/Lesson_007/calc_seminar/check.py
@@ -2,16 +2,6 @@ import math_f
 
 
 def check(data):
-    # if data[1] == '+':
-    #     return math_f.summa(data[0], data[2])
-    # elif data[1] == '-':
-    #     return math_f.sub(data[0], data[2])
-    # elif data[1] == '*':
-    #     return math_f.mult(data[0], data[2])
-    # elif data[1] == '/':
-    #     return math_f.dev(data[0], data[2])
-    # else:
-    #     print('something wrong')
     match data[1]:
         case '+':
             return math_f.summa(data[0], data[2])
